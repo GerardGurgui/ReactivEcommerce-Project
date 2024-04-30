303359eafdb53f224f6c792e3908e8f8ff99443c
GerardGurgui <gerardgurgui1991@gmail.com>
2024-04-30 20:02:37 +0200

    update branch master

---
+++ ProductCatalog-service/src/test/java/ReactiveEcommerce/ProductCatalog_service/ProductCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ReactiveEcommerce.ProductCatalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
