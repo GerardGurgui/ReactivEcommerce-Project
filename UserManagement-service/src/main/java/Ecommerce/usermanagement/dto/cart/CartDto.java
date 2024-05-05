ba5c49937413e61b18def15e1bc6a747e588cf44
GerardGurgui <gerardgurgui1991@gmail.com>
2024-05-05 20:15:01 +0200

    1- guardar lista de carritos a un usuario, a través de webclient con usermanagement. 2- metodos createCartForUser y updateActiveCartForUserAndSaveCart 3- creacion de DTOS adecuadas para este proposito, datos entre microservicios. por ejemplo, clase UserCartDto que contiene tanto el UserDto como el CartDto. 4- corrección de checks en cart service con mono<void> y mono.error 5-getUserInfo ahora devuelve tambien lista de carros del user 6- Eureka desactivada temporalmente para pruebas rapidas

---
+++ UserManagement-service/src/main/java/Ecommerce/usermanagement/dto/cart/CartDto.java
@@ -0,0 +1,26 @@
+package Ecommerce.usermanagement.dto.cart;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartDto {
+
+    private String userUuid;
+
+    @Pattern(regexp = "^[a-zA-Z0-9 ]*$")
+    @Size(max = 20, message = "Name must be a maximum than 20 characters")
+    private String name;
+
+    private int totalProducts;
+    private double totalPrice;
+
+
+}
