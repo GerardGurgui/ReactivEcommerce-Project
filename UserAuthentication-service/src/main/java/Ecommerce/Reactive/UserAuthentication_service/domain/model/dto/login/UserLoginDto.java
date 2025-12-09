da2be10cbc3b1276d739fb1da869b09448ab05b6
GerardGurgui <gerardgurgui1991@gmail.com>
2025-12-09 17:53:26 +0100

    Enhance user DTOs with validation constraints and regexp for registration and login

--- UserAuthentication-service/src/main/java/Ecommerce/Reactive/UserAuthentication_service/domain/model/dto/login/UserLoginDto.java
+++ UserAuthentication-service/src/main/java/Ecommerce/Reactive/UserAuthentication_service/domain/model/dto/login/UserLoginDto.java
@@ -11,7 +11,7 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserLoginDto{
+public class UserLoginDto {
 
     private String uuid;
     private String username;
